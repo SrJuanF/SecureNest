2d2ebc630a4b33c91da0648cb9d23d42be14cbe1
Mehmet Berat Öztürk <mehmet.ozturk@avalabs.org>
2025-04-11 16:33:10 +0300

    feat: implements reown integration (#7)

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,27 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
 import { Bounce, ToastContainer } from "react-toastify";
-import { WagmiProvider } from "wagmi";
-import { config } from "./config";
-
-const queryClient = new QueryClient();
+import { AppKitProvider } from "./AppKitProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<WagmiProvider config={config}>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ToastContainer
-				position="top-right"
-				autoClose={5000}
-				hideProgressBar
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover={false}
-				theme="dark"
-				transition={Bounce}
-				toastClassName={"font-mono border-cyber-green/60"}
-			/>
-			<Analytics />
-		</QueryClientProvider>
-	</WagmiProvider>,
+  <AppKitProvider>
+    <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover={false}
+      theme="dark"
+      transition={Bounce}
+      toastClassName={"font-mono border-cyber-green/60"}
+    />
+    <Analytics />
+  </AppKitProvider>
 );
